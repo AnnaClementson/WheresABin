dd9c83f681d3a058ad68fb6a1c72683f9f705119
anna clementson <annaclementson@hotmail.co.uk>
2021-11-06 09:57:47 +0000

    Add project files.

---
+++ WheresABin/Pages/Views/Location.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WheresABin.Pages
+{
+    public class LocationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WheresABin/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿
+
+
+function initMap() {
+    //Map options
+    var options = {
+        zoom: 8,
+        center: {lat:50.2195,lng:-4.8910}
+    }
+    //New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    //Add marker
+    var marker = new google.maps.Marker({
+        position: { lat: 50.2632, lng: -5.0510 },
+        map: map
+    });
+}
+
