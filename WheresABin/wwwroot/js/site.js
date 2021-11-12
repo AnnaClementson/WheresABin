d609d1bb8175ebce772ed8eb2ff2f9ef5f61eba2
anna clementson <annaclementson@hotmail.co.uk>
2021-11-12 10:59:35 +0000

    current location button added

--- WheresABin/wwwroot/js/site.js
+++ WheresABin/wwwroot/js/site.js
@@ -1,9 +1,16 @@
 ﻿
-$(document).ready(function () {
 
-})
+
+let map, infoWindow;
 
 function initMap() {
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+
+    infoWindow = new google.maps.InfoWindow();
 
     const iconBase =
         "https://developers.google.com/maps/documentation/javascript/examples/full/images/";
@@ -19,21 +26,7 @@ function initMap() {
         }
     };
 
-    //Map options
-    var options = {
-        zoom: 8,
-        center: { lat: 50.2195, lng: -4.8910 }
-    }
-    //New map
-    var map = new google.maps.Map(document.getElementById('map'), options);
-
-    //Listen for click on the map 
-    google.maps.event.addListener(map, 'click', function (event) {
-        //Add marker
-        addMarker({ coords: event.latLng });
-    });
-
-    //Array of markers
+     // Array of markers
     var markers = [
         {
             coords: { lat: 50.2632, lng: -5.0510 },
@@ -70,88 +63,84 @@ function initMap() {
         }
     ];
 
-    //Loop through markers
+      //Loop through markers
     for (var i = 0; i < markers.length; i++) {
         addMarker(markers[i]);
     }
 
-    //Add marker function 
-    function addMarker(props) {
-        var marker = new google.maps.Marker({
-            position: props.coords,
-            map: map,
-            //icon: props.iconImage
-        });
-        //Check for custom icon
-        if (props.iconImage) {
-            //Set icon image
-            marker.setIcon(props.iconImage);
-        }
+    //Listen for click on the map 
+    google.maps.event.addListener(map, 'click', function (event) {
+        //Add marker
+        addMarker({ coords: event.latLng });
+    });
 
-        //Check if there is content set, if so show info window
-        if (props.content) {
-            var infoWindow = new google.maps.InfoWindow({
-                content: props.content
-            });
+    const locationButton = document.createElement("button");
 
-            marker.addListener('click', function () {
-                infoWindow.open({
-                    anchor: marker,
-                    map,
-                    shouldFocus: false
-                });
-            })
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("Location found.");
+                    infoWindow.open(map);
+                    map.setCenter(pos);
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
         }
+    });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.open(map);
+}
+
+//Add marker function 
+function addMarker(props) {
+    var marker = new google.maps.Marker({
+        position: props.coords,
+        map: map,
+        animation: google.maps.Animation.DROP
+        //icon: props.iconImage
+    });
+    //Check for custom icon
+    if (props.iconImage) {
+        //Set icon image
+        marker.setIcon(props.iconImage);
     }
 
-    const contentString =
-        '<div id="content">' +
-        '<div id="siteNotice">' +
-        "</div>" +
-        '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-        '<div id="bodyContent">' +
-        "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-        "sandstone rock formation in the southern part of the " +
-        "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-        "south west of the nearest large town, Alice Springs; 450&#160;km " +
-        "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-        "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-        "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-        "Aboriginal people of the area. It has many springs, waterholes, " +
-        "rock caves and ancient paintings. Uluru is listed as a World " +
-        "Heritage Site.</p>" +
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-        "https://en.wikipedia.org/w/index.php?title=Uluru</a> " +
-        "(last visited June 22, 2009).</p>" +
-        "</div>" +
-        "</div>";
-
-    const infowindow = new google.maps.InfoWindow({
-        content: contentString,
-    });
-
-    marker.addListener("click", () => {
-        infowindow.open({
-            anchor: marker,
-            map,
-            shouldFocus: false,
+    //Check if there is content set, if so show info window
+    if (props.content) {
+        var infoWindow = new google.maps.InfoWindow({
+            content: props.content
         });
-    });
 
-
-    ////Add marker
-    //var marker = new google.maps.Marker({
-    //    position: { lat: 50.2632, lng: -5.0510 },
-    //    map: map,
-    //    icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    //});
-
-    //var infoWindow = new google.maps.InfoWindow({
-    //    content:'<h1>Truro</h1>'
-    //});
-
-    //marker.addListener('click', function () {
-    //    infoWindow.open(map, marker);
-    //})
-
+        marker.addListener('click', function () {
+            infoWindow.open({
+                anchor: marker,
+                map,
+                shouldFocus: false
+            });
+        })
+    }
 }
