4e9ee4f3a9db8be90aa2b4ac2c0738f094b2e154
anna clementson <annaclementson@hotmail.co.uk>
2021-11-25 13:38:56 +0000

    Added map filter and bootstrap layout

---
+++ WheresABin/wwwroot/js/bin.js
@@ -0,0 +1,39 @@
+﻿
+const bin = [
+    {
+        "X": 60037,
+        "Y": 34563,
+        "OBJECTID": 1,
+        "CATEGORY": "Waste Bin",
+        "NAME": "Bin number 1",
+        "ADDRESS": "2 west avenue",
+        "WHATTHREEWORDS": "Yellow mark spoon",
+        "COMMENTS": "This is a black bin",
+        "LONGITUDE": -4.143841,
+        "LATITUDE": 50.376289
+    },
+    {
+        "X": 32253,
+        "Y": 34343,
+        "OBJECTID": 2,
+        "CATEGORY": "Dog Bin",
+        "NAME": "Bin number 2",
+        "ADDRESS": "2 bark road",
+        "WHATTHREEWORDS": "Green fox dread",
+        "COMMENTS": "This is a dog bin",
+        "LONGITUDE": -0.118092,
+        "LATITUDE": 51.509865
+    },
+    {
+        "X": 45642,
+        "Y": 34443,
+        "OBJECTID": 3,
+        "CATEGORY": "Green Bin",
+        "NAME": "Bin number 2",
+        "ADDRESS": "2 bark road",
+        "WHATTHREEWORDS": "Yell fox dread",
+        "COMMENTS": "This is a green bin",
+        "LONGITUDE": -5.0610,
+        "LATITUDE": 50.2452,
+    }
+]
